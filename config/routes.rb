325fdbc6cb25e15cc6189b173e21dcb660ccc237
renaud lourgouilloux lourgouilloux.renaud@gmail.com <lourgouilloux.renaud@gmail.com>
2024-06-25 14:45:39 +0200

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-  get 'lists/create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/destroy'
-  get 'lists/all'
-  root "lists#index"
+  root to: 'lists#index'
 
-  resources :lists
-  resources :bookmarks
-  resources :movies
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
