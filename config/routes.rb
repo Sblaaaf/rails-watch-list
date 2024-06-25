abfc4fdc37d49c25c3d49cf81c3754a4e4e5363e
renaud lourgouilloux lourgouilloux.renaud@gmail.com <lourgouilloux.renaud@gmail.com>
2024-06-25 14:44:07 +0200

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/create'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/destroy'
+  get 'lists/all'
+  root "lists#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :lists
+  resources :bookmarks
+  resources :movies
 end
